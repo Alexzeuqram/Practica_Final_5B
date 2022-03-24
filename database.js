3e729b1c1e70aebf50cabdf3394ef2986a32fa6a
Alexzeuqram <jared.marquez.is@unipolidgo.edu.mx>
2022-03-24 00:05:47 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 let config = {
-	host: "localhost",
-	user: "root",
-	password: "",
+	host: "us-cdbr-east-05.cleardb.net",
+	user: "bb613e290e6b71",
+	password: "5a8b27a7",
 	port: 3306,
-	database: "adoption_system",
+	database: "heroku_7b5ce8912e208fb",
 	secret_key: "Jared2211kk2"
 };
 exports.config = config;
