1140dc435ca5468062a5a078a4f7075cf208c4c3
Alexzeuqram <jared.marquez.is@unipolidgo.edu.mx>
2022-06-20 12:10:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ userRoutes = require("./routes/user");
 petRoutes = require("./routes/pet");
 adoptionRoutes = require("./routes/adoption");
 
+//Manuel zzzz
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(myConnection(mysql, db));
 app.use("/user", userRoutes);
